9f3b9717e34f7cc7cb2c2b83928bd00ce973dec5
azerty <kenokech94@gmail.com>
2020-11-06 06:26:45 +0100

    WIP

--- terminal/troubleshoot/LED.py
+++ terminal/troubleshoot/LED.py
@@ -1,11 +1,19 @@
-import RPi.GPIO as GPIO
+import os
 import time
+
+import RPi.GPIO as GPIO
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LED_PIN = os.getenv("LED_PIN")
+assert LED_PIN is not None
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT)
 print("LED on")
-GPIO.output(18, GPIO.HIGH)
+GPIO.output(LED_PIN, GPIO.HIGH)
 time.sleep(1)
 print("LED off")
 GPIO.output(18, GPIO.LOW)
